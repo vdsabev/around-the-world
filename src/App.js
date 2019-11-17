7e30dbf80d66182622992f93cd28a2c9c3db4aed
Vladimir Sabev <vdsabev@gmail.com>
2019-11-17 14:04:16 +0200

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
+import theme from './theme';
+import ErrorBoundary from './ErrorBoundary';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <ErrorBoundary>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </AppHeader>
+      </ErrorBoundary>
+    </AppContainer>
   );
-}
+};
 
 export default App;
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+`;
+
+const AppHeader = styled.header`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 100vh;
+  background: ${theme.primary.main};
+  color: ${theme.primary.inverse};
+  font-size: calc(10px + 2vmin);
+`;
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`,
+  primary: {
+    main: '#2d3436',
+    inverse: 'white',
+  },
+};
+
+export default theme;
